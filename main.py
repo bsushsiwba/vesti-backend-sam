00332b105b1e43f0a1cfa4adc6b4bc258b39ce0b
bsushsiwba <66201863+bsushsiwba@users.noreply.github.com>
2025-05-31 13:15:01 +0500

    Revert "added install command to file"
    This reverts commit 5e01fe4d167e813a8c88e23a7cf52da7f33e73df.

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-import os
-
-os.system(
-    "pip install torch==2.5.1 && pip install packaging hydra-core scikit-learn wheel iopath onnxruntime rembg && pip install transformers==4.45.2 && pip install git+https://github.com/bsushsiwba/grounded-sam-pypi.git"
-)
-print("Dependencies installed successfully.")
-
 from autodistill_grounded_sam_2 import GroundedSAM2
 from autodistill.detection import CaptionOntology
 import os
@@ -19,8 +12,6 @@ base_model = GroundedSAM2(
         }
     )
 )
-
-print("GroundedSAM2 model initialized successfully.")
 
 # read flag.txt or initialize with 1
 with open("flag.txt", "w") as f:
