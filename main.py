5e01fe4d167e813a8c88e23a7cf52da7f33e73df
bsushsiwba <66201863+bsushsiwba@users.noreply.github.com>
2025-05-31 13:06:51 +0500

    added install command to file

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+import os
+
+os.system(
+    "pip install torch==2.5.1 && pip install packaging hydra-core scikit-learn wheel iopath onnxruntime rembg && pip install transformers==4.45.2 && pip install git+https://github.com/bsushsiwba/grounded-sam-pypi.git"
+)
+print("Dependencies installed successfully.")
+
 from autodistill_grounded_sam_2 import GroundedSAM2
 from autodistill.detection import CaptionOntology
 import os
@@ -12,6 +19,8 @@ base_model = GroundedSAM2(
         }
     )
 )
+
+print("GroundedSAM2 model initialized successfully.")
 
 # read flag.txt or initialize with 1
 with open("flag.txt", "w") as f:
